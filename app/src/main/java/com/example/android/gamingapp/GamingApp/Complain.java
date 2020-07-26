3b5c0186125ffa24c256c8482ec5afd9104fae25
ankit-bws <50819073+ankit-bws@users.noreply.github.com>
2020-07-26 21:39:17 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/android/gamingapp/GamingApp/Complain.java
@@ -0,0 +1,15 @@
+package com.example.android.gamingapp.GamingApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.gamingapp.R;
+
+public class Complain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain);
+    }
+}
